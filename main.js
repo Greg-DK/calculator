a5cc016703e9ac16c8f63418acd173def5d87555
Greg-DK <67904364+Gdekwant@users.noreply.github.com>
2022-12-29 20:33:23 +1000

    mostly complete calculator, no error checking or string checking

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let history = document.getElementById("history")
 let firstInput = "0";
 let secondInput = 0;
 let operatorInput = 0;
+let result = 0;
 let number =["0","1","2","3","4","5","6","7","8","9","decimal"]
 let operator =["add", "minus", "multiply", "divide"]
 
@@ -16,30 +17,60 @@ for (let i = 0; i < buttons.length; i++) {
 
 function calculate(button) {
     let input = button.target.id;
-    console.log(input)
     if (input==="add" || input==="minus" || input==="multiply" || input==="divide") {
-        // current = 0;
-        operatorInput = input;
-        history.innerText = firstInput + ' ' + operatorInput; // convert operatorInput to the icon when printed to history.innertext
-        current.innerText = "0";
-        console.log(operatorInput)
+        if (operatorInput === 0) {
+            operatorInput = input;
+            history.innerText = firstInput // convert operatorInput to the icon when printed to history.innertext
+            current.innerText = "0";
+        } else {
+            if (operatorInput === "add") {
+                result = parseFloat(firstInput) + parseFloat(secondInput);
+            } else if (operatorInput === "multiply") {
+                result = parseFloat(firstInput) * parseFloat(secondInput);
+            } else if (operatorInput === "divide") {
+                result = parseFloat(firstInput) / parseFloat(secondInput);
+            } else if (operatorInput === "minus") {
+                result = parseFloat(firstInput) - parseFloat(secondInput);
+            }
+            history.innerText = history.innerText.concat(' ').concat(button.target.innerText).concat(' ').concat(secondInput);
+            history.innerText = history.innerText.replace(' 0 ', '');
+            current.innerText = result;
+            firstInput = result.toString();
+            secondInput = "0";
+            operatorInput = input;
+        }
+
+        // if (current.innerText != "0") {
+            
+        // }
         // use endswith() to check if the last digit is "." if so append 2x0's
     }
 
     if (input === "decimal") {
-        for (let i=0; i<firstInput.length; i++) {
-            if (firstInput[i] === "\.") {
-            return;
-            }   
+        if (history.innerText ==="0") {    
+            for (let i=0; i<firstInput.length; i++) {
+                if (firstInput[i] === "\.") {
+                return;
+                }   
+            }
+            firstInput = firstInput.concat("\.");
+            current.innerText = firstInput; 
         }
-        firstInput = firstInput.concat("\.");
-        current.innerText = firstInput; 
+        else {
+            for (let i=0; i<secondInput.length; i++) {
+                if (secondInput[i] === "\.") {
+                return;
+                }   
+            }
+            secondInput = secondInput.concat("\.");
+            current.innerText = secondInput; 
+        }
     }
 
     if (input === "clear") {
         firstInput = "0";
         secondInput = "0";
-        operatorInput = "0";
+        operatorInput = 0;
         current.innerText = "0";
         history.innerText = "0";
     }
@@ -55,6 +86,13 @@ function calculate(button) {
             }
             current.innerText = firstInput;
         }
+        else {
+            secondInput = secondInput.slice(0,-1);
+            if (secondInput === '') {
+                secondInput = "0";
+            }
+            current.innerText = secondInput;
+        }
         //remove the last digit
     }
 
@@ -64,20 +102,32 @@ function calculate(button) {
             if (firstInput[0] === "0") {
                 firstInput = firstInput.slice(1);
             }
-            console.log(firstInput)
             current.innerText = firstInput;
-            //add a display to screen
         }
-        // else {
-        //     secondInput += input;
-        //     current = secondInput;
-            //add a display to screen
-        // }
+        else {
+            secondInput = secondInput+input;
+            if (secondInput[0] === "0") {
+                secondInput = secondInput.slice(1);
+            }
+            current.innerText = secondInput;
+        }
     }
 
-    // if (input = equals) {
-    //     current = firstInput Math(operator) secondInput; //fix logic for maths
-    //     //add a display to screen
-    // }
+    if (input === "equals") {
+        if (operatorInput === "add") {
+            result = parseFloat(firstInput) + parseFloat(secondInput);
+        } else if (operatorInput === "multiply") {
+            result = parseFloat(firstInput) * parseFloat(secondInput);
+        } else if (operatorInput === "divide") {
+            result = parseFloat(firstInput) / parseFloat(secondInput);
+        } else if (operatorInput === "minus") {
+            result = parseFloat(firstInput) - parseFloat(secondInput);
+        }
+        history.innerText = history.innerText.concat(' ').concat(secondInput);
+        current.innerText = result;
+        firstInput = result.toString();
+        secondInput = "0";
+        operatorInput = 0;
+    }
 
 }
